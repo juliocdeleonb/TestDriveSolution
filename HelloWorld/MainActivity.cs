2fabb84720f40231c21a2bed385d840feb2db3a2
Julio De León <juliocdeleonb@gmail.com>
2014-04-20 00:21:24 -0500

    Added AlertDialog and TextView

--- HelloWorld/MainActivity.cs
+++ HelloWorld/MainActivity.cs
@@ -13,20 +13,42 @@ namespace HelloWorld
 	{
 		int count = 1;
 
+		AlertDialog ActivityDialog;
+
 		protected override void OnCreate (Bundle bundle)
 		{
 			base.OnCreate (bundle);
+
+			// Create the Dialog object used to display messages
+			AlertDialog.Builder builder = new AlertDialog.Builder(this);
+			ActivityDialog = builder.Create ();
+			ActivityDialog.SetTitle("HelloWorld");
+			ActivityDialog.DismissEvent += (sender, e) => {
+				FindViewById<TextView> (Resource.Id.textViewMessage).Text = "Alert dismissed at " + DateTime.Now.ToLongTimeString();
+			};
 
 			// Set our view from the "main" layout resource
 			SetContentView (Resource.Layout.Main);
 
 			// Get our button from the layout resource,
 			// and attach an event to it
-			Button button = FindViewById<Button> (Resource.Id.myButton);
+			Button buttonClicker = FindViewById<Button> (Resource.Id.myButton);
 			
-			button.Click += delegate {
-				button.Text = string.Format ("{0} clicks!", count++);
+			buttonClicker.Click += delegate {
+				buttonClicker.Text = string.Format ("{0} clicks!", count++);
 			};
+
+			FindViewById<Button> (Resource.Id.buttonPopupMessage).Click += buttonPopupMessage_Click;
+		}
+
+		private void buttonPopupMessage_Click(object sender, EventArgs e)
+		{
+			ActivityDialog.SetMessage ("Hello Android World");
+			ActivityDialog.SetButton ("OK", (s, a) => {
+				((Button)sender).Text = "Say hello again :3";
+			});
+
+			ActivityDialog.Show ();
 		}
 	}
 }
--- HelloWorld/Resources/Resource.designer.cs
+++ HelloWorld/Resources/Resource.designer.cs
@@ -60,8 +60,14 @@ namespace HelloWorld
 		public partial class Id
 		{
 			
+			// aapt resource value: 0x7f050001
+			public const int buttonPopupMessage = 2131034113;
+			
 			// aapt resource value: 0x7f050000
 			public const int myButton = 2131034112;
+			
+			// aapt resource value: 0x7f050002
+			public const int textViewMessage = 2131034114;
 			
 			static Id()
 			{
@@ -92,11 +98,17 @@ namespace HelloWorld
 		public partial class String
 		{
 			
-			// aapt resource value: 0x7f040001
-			public const int app_name = 2130968577;
-			
 			// aapt resource value: 0x7f040000
-			public const int hello = 2130968576;
+			public const int Hello = 2130968576;
+			
+			// aapt resource value: 0x7f040001
+			public const int PopMessage = 2130968577;
+			
+			// aapt resource value: 0x7f040003
+			public const int TextViewMessage = 2130968579;
+			
+			// aapt resource value: 0x7f040002
+			public const int app_name = 2130968578;
 			
 			static String()
 			{
